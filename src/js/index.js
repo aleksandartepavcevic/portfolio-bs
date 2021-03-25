edacf182b043bf12f76e9425c9ed2aa5ad2fca79
aleksandartepavcevic <atepavcevic29@gmail.com>
2021-03-25 21:38:02 +0100

    responsive

---
+++ src/js/index.js
@@ -0,0 +1,52 @@
+$(window).on('beforeunload', function () {
+  $(window).scrollTop(0);
+});
+
+$('a[href*="#"]')
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function (event) {
+    if (
+      location.pathname.replace(/^\//, '') ==
+        this.pathname.replace(/^\//, '') &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        event.preventDefault();
+        $('html, body').animate(
+          {
+            scrollTop: target.offset().top,
+          },
+          1000,
+          function () {
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(':focus')) {
+              return false;
+            }
+          }
+        );
+      }
+    }
+  });
+
+function isScrolledIntoView(elem) {
+  var docViewTop = $(window).scrollTop();
+  var docViewBottom = docViewTop + $(window).height();
+
+  var elemTop = $(elem).offset().top;
+  var elemBottom = elemTop + $(elem).height();
+
+  return elemBottom <= docViewBottom && elemTop >= docViewTop;
+}
+
+$(window).scroll(function () {
+  $('.do-animation').each(function () {
+    if (isScrolledIntoView(this) === true) {
+      $(this).removeClass('do-animation');
+      $(this).addClass('animate');
+    }
+  });
+});
